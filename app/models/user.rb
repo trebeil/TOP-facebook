71811087f8909bcde7c3bb6754b0156f004bdae6
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-27 14:45:16 -0300

    Adds association between user and posts and validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable, :timeoutable
+
+  has_many :posts, inverse_of: 'author', dependent: :destroy
+  validates :name, :last_name, :email, presence: true
 end
