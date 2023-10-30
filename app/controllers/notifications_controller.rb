26c03ebfb717659c67d508e8eb1e32903d93ec5d
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-30 11:07:49 -0300

    Allow deleting notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,10 +1,7 @@
 class NotificationsController < ApplicationController
-  def index
-  end
-
-  def create
-  end
-
   def destroy
+    @notification = Notification.find(params[:id])
+    @notification.destroy
+    redirect_back_or_to :root
   end
 end
