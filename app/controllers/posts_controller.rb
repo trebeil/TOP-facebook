957caf1153d6f83ad550987f95ff176d962d9803
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-30 14:11:04 -0300

    Allow creating Posts and viewing own' and friend's posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,32 @@
 class PostsController < ApplicationController
   def index
+    friends_ids = Friendship.where(user_id: current_user.id, status: 2)
+                            .pluck(:friend_id)
+    @posts = Post.where(user_id: current_user.id)
+                 .or(Post.where(user_id: friends_ids))
+                 .order(created_at: :desc).limit(10)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
   end
 end
