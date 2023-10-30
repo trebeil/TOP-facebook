51d791f0526eb29ee58dad599f91a1ce183a5ba7
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-30 15:02:54 -0300

    Allow deleting own posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_back_or_to :root
+  end
+
   private
 
   def post_params
