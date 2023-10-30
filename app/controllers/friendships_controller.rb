fe5e76f03ba179cf82e02c5410a8306fb6018720
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-29 22:33:41 -0300

    Allow canceling a friend request on the friend's profile page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -20,5 +20,12 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
+    @friendship = Friendship.find(params[:id])
+    @friendship2 = Friendship.find_by(user_id: @friendship.friend_id, 
+                                      friend_id: @friendship.user_id)
+    @friend = @friendship.friend
+    @friendship.destroy
+    @friendship2.destroy
+    redirect_to @friend
   end
 end
