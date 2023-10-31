63f57371f62bcf7db8de2ee4d5d4973c945528ac
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-30 23:00:35 -0300

    Create form for comments on posts and define comment#create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_back_or_to :root
+    else
+      flash[:error] = 'Invalid comment'
+      redirect_back_or_to :root
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
 end
