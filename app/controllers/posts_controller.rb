ab3a57d568e8fcfb35a67e19caa22e43767b7d99
Daniel Liebert <daniel.liebert@gmail.com>
2023-11-13 09:29:45 -0300

    Filter posts#show view for only own and friends' posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :check_authorization, only: :destroy
+  before_action :check_authorization_for_destroy, only: :destroy
+  before_action :check_authorization_for_show, only: :show
 
   def index
     friends_ids = Friendship.where(user_id: current_user.id, status: 2)
@@ -49,9 +50,16 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content)
   end
 
-  def check_authorization
+  def check_authorization_for_destroy
     post = Post.find(params[:id])
     unless post.author == current_user
+      redirect_back_or_to :root, status: 403
+    end
+  end
+
+  def check_authorization_for_show
+    post = Post.find(params[:id])
+    unless post.author == current_user || post.author.friends.exists?(id: current_user.id)
       redirect_back_or_to :root, status: 403
     end
   end
