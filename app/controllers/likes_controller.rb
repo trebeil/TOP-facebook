c1b71160bd1531b4c4f7ed744b9fbaa3fe53827d
Daniel Liebert <daniel.liebert@gmail.com>
2023-11-15 11:07:09 -0300

    Use turbo streams for liking and unliking

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,30 +1,38 @@
 class LikesController < ApplicationController
-  before_action :check_authorization, only: :destroy
+  before_action :check_authorization_for_create, only: :create
+  before_action :check_authorization_for_destroy, only: :destroy
 
   def create
-    like = Like.new(like_params)
-    like.user_id = current_user.id
-    like.save
+    @like = Like.new(like_params)
+    @like.user_id = current_user.id
+    @like.save
 
     # Create notification unless like author is the post/comment author
-    unless like.user_id == like.likeable.user_id
-      path = if like.likeable_type == 'Post'
-               post_path(like.likeable.id)
+    unless @like.user_id == @like.likeable.user_id
+      path = if @like.likeable_type == 'Post'
+               post_path(@like.likeable.id)
              else
-               post_path(like.likeable.post.id)
+               post_path(@like.likeable.post.id)
              end
-      Notification.create(notificationable_id: like.id,
+      Notification.create(notificationable_id: @like.id,
                           notificationable_type: 'Like',
-                          user_id: like.likeable.author.id,
-                          text: notification_text(like),
+                          user_id: @like.likeable.author.id,
+                          text: notification_text(@like),
                           path: path)
     end
-    redirect_back_or_to :root
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back_or_to :root }
+    end
   end
 
   def destroy
-    Like.destroy(params[:id])
-    redirect_back_or_to :root
+    @like = Like.find(params[:id])
+    @like.destroy
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back_or_to :root }
+    end
   end
 
   private
@@ -41,7 +49,27 @@ class LikesController < ApplicationController
     params.require(:like).permit(:likeable_id, :likeable_type)
   end
 
-  def check_authorization
+  # Allow liking only
+  #   - resources (comments or posts) whose author is the current_user or one of current_user's friends
+  #   - comments that belong to posts whose author is the current_user or one of current_user's friends
+  def check_authorization_for_create
+    like = Like.new(like_params)
+    resource = like.likeable
+    allow_create = false
+    allowed_list = current_user.friends << current_user
+
+    allow_create = true if allowed_list.include?(resource.author)
+
+    if resource.instance_of?(Comment)
+      allow_create = true if allowed_list.include?(resource.post.author)
+    end
+
+    unless allow_create
+      redirect_back_or_to :root, status: 403
+    end
+  end
+
+  def check_authorization_for_destroy
     like = Like.find(params[:id])
     unless like.user_id == current_user.id
       redirect_back_or_to :root, status: 403
