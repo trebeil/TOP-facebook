4c1a47c5882e8e16e137cbf15f00dbe62b4efb91
Daniel Liebert <daniel.liebert@gmail.com>
2023-11-17 15:46:59 -0300

    Update seeds to create users with profile photo attached

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+['a','b','c','d','e','f'].each do |l|
+  User.create(email: "#{l}@#{l}.com",
+              password: 'password',
+              password_confirmation: 'password',
+              name: "#{l}#{l}#{l}".capitalize,
+              last_name: "#{l}#{l}#{l}#{l}".capitalize)
+end
+
+User.all.each do |user|
+  uri = 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/2026.png'
+  user.photo.attach(io: URI.parse(uri).open, filename: "profile.#{uri.split('.').last}")
+end
+
+50.times do
+  Post.create(user_id: rand(1..6), content: Array('a'..'z').sample(14).join)
+end
