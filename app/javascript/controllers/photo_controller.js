0bbd525bf05bc78ae43d97495f608c1fa7ad8130
Daniel Liebert <daniel.liebert@gmail.com>
2024-02-08 00:26:36 -0300

    Improve UX and frontend for 2nd step of sign up process

--- app/javascript/controllers/photo_controller.js
+++ app/javascript/controllers/photo_controller.js
@@ -8,8 +8,6 @@ export default class extends Controller {
   }
 
   preview() {
-    this.element.removeChild(this.imageTarget)
-    
     const files = this.inputTarget.files
 
     if (files.length === 0) {
@@ -18,14 +16,14 @@ export default class extends Controller {
       image.style.maxHeight = '90px';
       image.style.maxWidth = '90px';
       image.src = this.originalSrc;
-      this.element.appendChild(image);
+      this.element.replaceChild(image, this.imageTarget);
     } else {
       const image = document.createElement('img');
       image.setAttribute('data-photo-target', 'image')
       image.style.maxHeight = '90px';
       image.style.maxWidth = '90px';
       image.src = URL.createObjectURL(files[0]);
-      this.element.appendChild(image);
+      this.element.replaceChild(image, this.imageTarget);
     }
   }
 }
